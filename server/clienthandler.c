945135b49b53b4fb1df3607bdb2bc14c8cac12f6
Dashbloxx <dashbloxx@gmail.com>
2023-05-31 15:30:25 -0900

    Project: Implement loop that checks for unexpected client disconnection

--- server/clienthandler.c
+++ server/clienthandler.c
@@ -13,6 +13,20 @@
 client_t clients[1024];
 int client_incrementer = 0;
 
+/* Check if a client is connected... */
+static bool is_client_connected(int client_socket) {
+    struct sockaddr_in client_addr;
+    socklen_t client_addrlen = sizeof(client_addr);
+    
+    if (getpeername(client_socket, (struct sockaddr*)&client_addr, &client_addrlen) == -1) {
+        /* Looks like an error happened while running getpeername()! Seems like the client isn't connected... */
+        perror("getpeername() failed");
+        return false;
+    }
+    
+    return true;
+}
+
 void* handle_client(void* arg) {
     client_incrementer++;
     int client_socket = *((int*)arg);
@@ -30,12 +44,20 @@ void* handle_client(void* arg) {
     clients[client_incrementer].port = ntohs(client_addr.sin_port);
     clients[client_incrementer].in_use = true;
 
-    printf("New client connected: %s:%d\n", client_ip, client_port);
+    printf("New client connected: %s:%d\n", clients[client_incrementer].ip_address, clients[client_incrementer].port);
 
-    
+    while(true) {
+        sleep(5);
+        if(is_client_connected(client_socket) == false) {
+            close(client_socket);
+            printf("Client disconnected: %s:%d\n", clients[client_incrementer].ip_address, clients[client_incrementer].port);
+            clients[client_incrementer].in_use = false;
+            pthread_exit(NULL);
+        }
+    }
 
     close(client_socket);
-    printf("Client disconnected: %s:%d\n", client_ip, client_port);
+    printf("Client disconnected: %s:%d\n", clients[client_incrementer].ip_address, clients[client_incrementer].port);
     clients[client_incrementer].in_use = false;
     pthread_exit(NULL);
 }
